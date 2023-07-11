78e49d937fb34fb27d232b489e2d3ada107dc8de
kushal <kushalbhetuwal@gmail.com>
2023-07-12 01:54:22 +1000

    added

--- kushal/src/App.js
+++ kushal/src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      hi, there this is just for practice
     </div>
   );
 }
